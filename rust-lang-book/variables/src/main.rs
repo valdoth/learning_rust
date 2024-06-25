f5db9aef09e59b997bf6edb952612d50bd2daf7f
NDIMBIARISOA Valdo Tsiaro Hasina <50540492+valdoth@users.noreply.github.com>
2024-06-25 13:16:50 +0000

    add gitignore and modify folder

--- rust-lang-book/variables/src/main.rs
+++ rust-lang-book/variables/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const SUBSCRIBER_COUNT: i32 = 100_000;
+    println!("{}", SUBSCRIBER_COUNT);
+
+    // Compound Types
+    let tup = ("Let's Get Rusty!", 100_000);
+    let (channel, sub_count) = tup;
+    println!("channel: {}, sub_count {}", channel, sub_count);
+
+    let error_codes = [200, 304, 500];
+    let not_found = error_codes[1];
+    let byte = [0; 8];
+    println!("error code not_found: {}", not_found);
+    println!("byte : {:?}", byte);
 }
