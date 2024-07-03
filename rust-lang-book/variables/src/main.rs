2758fddba63f8b96cb5c7e846246d5ff0b72ca1f
valdoth <valdotsiarohasina@gmail.com>
2024-07-03 20:46:47 +0300

    variables done

--- rust-lang-book/variables/src/main.rs
+++ rust-lang-book/variables/src/main.rs
@@ -17,4 +17,37 @@ fn main() {
     let byte = [0; 8];
     println!("error code not_found: {}, others code errors: {:?}", not_found, error_codes);
     println!("byte : {:?}", byte);
+
+    let result = my_function(11, 22);
+    println!("The function result is {}", result);
+
+    // control flow
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value number is {} when condition is {}", condition, number);
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter;
+        }
+    };
+    println!("result is {}", result);
+
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("The value is: {}", element);
+    }
+
+    for number in 1..4 {
+        println!("{}!", number);
+    }
+}
+
+fn my_function(x: i32, y: i32) -> i32 {
+    println!("Another function.");
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    x + y
 }
